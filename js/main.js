7cedc7c49c60e0a18f4aeb00ac8fe641643b75a9
Jesper Gyldenløve <jespergyldenlve@gmail.com>
2019-05-21 10:25:34 +0200

    Website huge Commit
    Commit = diverse billeder, meste af platformen til mobildelen af nordic room

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
 //commit
+
+// $("a").click(function(){
+//   return false;
+// })
+$(".kunstner").click(function(){
+  $(this).toggleClass("k-aktiv");
+})
+//event-click for navigation
+$("nav div").click(function(){
+  $("nav ul").toggleClass("open");
+});
+$("nav ul li a").click(function(){
+  if ($("nav ul").hasClass("open")) {
+    $("nav ul").removeClass("open");
+  }
+})
+
+$(".udvid").click(function(){
+  var numItems = $('.gallery div').length - 1;
+  console.log(numItems);
+  var heightItems = numItems * 30 + "vh";
+  console.log(heightItems);
+var attr = $(".gallery").attr('style');
+
+  if (typeof attr !== typeof undefined && attr !== false) {
+    $(".gallery").removeAttr("style", "height");
+    $(".udvid").toggleClass("luk");
+    console.log("shiet")
+  } else {
+  $(".udvid").toggleClass("luk");
+    $(".gallery").css("height", heightItems);
+  }
+})
+
+
+$(document).ready(function(){
+$('.slider').slick({
+  lazyLoad: 'ondemand',
+  infinite: true,
+  dots: true,
+  arrows: false
+});
+});
