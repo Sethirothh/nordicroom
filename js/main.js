83b6af66fa166946c61df4cb9eae32f827681e45
Jesper Gyldenløve <jespergyldenlve@gmail.com>
2019-05-21 13:45:30 +0200

    Småjusteringer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 //commit
 
-// $("a").click(function(){
-//   return false;
-// })
 $(".kunstner").click(function(){
   $(this).toggleClass("k-aktiv");
 })
@@ -16,10 +13,10 @@ $("nav ul li a").click(function(){
   }
 })
 
+
 $(".udvid").click(function(){
   var numItems = $('.gallery div').length - 1;
-  var heightItems = numItems * 31.3 + "vh";
-  console.log(heightItems);
+  var heightItems = numItems * 36.8 + "vh";
   var attr = $(".gallery").attr('style');
   if (typeof attr !== typeof undefined && attr !== false) {
     $(".gallery").removeAttr("style", "height");
@@ -31,6 +28,31 @@ $(".udvid").click(function(){
   }
 })
 
+$(window).scroll(function(){
+  stickyScroll()});
+
+  var navbar = $("nav");
+  var sticky = navbar.offsetTop;
+
+  function stickyScroll(){
+    if (window.pageYOffset >= sticky) {
+      $("nav").addClass("sticky")
+    } else {
+      $("nav").removeClass("sticky");
+    }
+  };
+  window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
 $(document).ready(function(){
 $('.slider').slick({
@@ -39,4 +61,6 @@ $('.slider').slick({
   dots: true,
   arrows: false
 });
+
+
 });
